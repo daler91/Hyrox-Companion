27c4459dca0a29c061bdc1814745a04f967e67c6
daler91 <52685879+daler91@users.noreply.github.com>
2026-05-03 16:49:48 -0500

    Expand structured health counter constraint for new metrics

---
+++ migrations/0039_structured_exercise_health_counter_expansion.sql
@@ -0,0 +1,21 @@
+ALTER TABLE "structured_exercise_health_counters"
+DROP CONSTRAINT IF EXISTS "structured_exercise_health_counter_name_check";
+
+ALTER TABLE "structured_exercise_health_counters"
+ADD CONSTRAINT "structured_exercise_health_counter_name_check"
+CHECK (
+  "structured_exercise_health_counters"."counter_name" IN (
+    'text_only_rows_detected',
+    'auto_hydration_attempted',
+    'auto_hydration_succeeded',
+    'auto_hydration_failed',
+    'manual_fix_completed',
+    'rejected_text_only_write',
+    'parse_text_attempted',
+    'parse_text_succeeded',
+    'parse_text_failed',
+    'parse_photo_attempted',
+    'parse_photo_succeeded',
+    'parse_photo_failed'
+  )
+);
